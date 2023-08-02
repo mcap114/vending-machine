d73948bc3bdf7dc2eea28c374bbc99b70cc56c14
Miguel <miguelcaparos114@gmail.com>
2023-08-03 00:19:24 +0800

    added some gui elements

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner;
 
 /**
@@ -6,6 +8,18 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Capasilog");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+
+        ImageIcon eggboy = new ImageIcon("src/eggboy.jpg");
+        frame.setIconImage(eggboy.getImage());
+        frame.getContentPane().setBackground(new Color(231, 226, 210));
+
         int slots;
         boolean exitAll = false;
         boolean specialActive = false;
